ed13430151883a23a19cfba7a595b1cf6e375eb1
Claire Wanjiru <clairewanjiru.m@gmail.com>
2024-05-31 22:10:22 +0300

    added string and array functions

--- script.js
+++ script.js
@@ -21,3 +21,31 @@ function capitalizeFirstLetter(sentence) {
 }
 let capitalizedSentence = capitalizeFirstLetter("i am a student at gomycode");
 console.log(capitalizedSentence);
+
+// Array Functions:
+// Find Maximum and Minimum: Write functions to find the maximum and minimum values in an array of numbers.
+let numbers = [5, 10, 15, 20, 25, 30];
+
+function findMaxMin(numbers) {
+  let max = numbers[0];
+  let min = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    } else if (numbers[i] < min) {
+      min = numbers[i];
+    }
+  }
+  return "Max: ${max}, Min: ${min}";
+}
+findMaxMin(numbers);
+
+// Sum of Array
+function sumAll(arr) {
+  let sum = 0;
+  for (let a of arr) sum += a;
+  return sum;
+}
+console.log(sumAll([5, 10, 15, 20]));
+
+// Filter Array
